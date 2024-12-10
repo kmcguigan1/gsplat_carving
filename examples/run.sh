9a249bb05b12f7fbefbb772965fef4ab3e51a915
Fernando961226 <fernandopena961226@gmail.com>
2024-12-10 11:05:51 -0500

    added file to run

---
+++ examples/run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=2 python simple_trainer.py default --disable_viewer --data_factor 8 \
+            --render_traj_path ellipse \
+            --data_dir data/360_v2/bonsai/ \
+            --result_dir results/benchmark/bonsai/ \
